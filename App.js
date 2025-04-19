34c3270298fc1d2d117a5cae766609eeefb68ab9
vijaydev549 <techvijaykumar.b@gmail.com>
2025-04-19 18:26:55 +0530

    Episode 3 Code

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
-const heading = React.createElement(
-  "div",
-  { id: "parent" },[
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "Hello H1 React Tag")
-  ),
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "Hello H1 React Tag")
-  )
-]
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Creating React Element using JSX
+const jsxHeading = <h1 id="root">"Namastey React Using Jsx"</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// Created FunctionalBased Component
+const HeadingComponent = () => {
+  return <h1>Namastey React Functional Based Compnent</h1>;
+};
+// Component Composition
+const HeadingComponent1 = () => (
+  <div id="container">
+    <HeadingComponent /> 
+<h1>Namastey React using FBC type2</h1>
+</div>
+);
+const legacy =React.createElement("h1",{id:"root"},"Namaskaara legacy way");
+const jsxWay =<h1 id="jsxheading">Namastey JSX</h1>;
+root.render(legacy);
+root.render(jsxWay);
+
+const FinalComponent=()=>(
+<div id="finalCall">
+ {legacy}
+ {HeadingComponent()}
+<HeadingComponent1></HeadingComponent1>
+<h1 id="finalRes">This is how all components works</h1>
+</div>);
+root.render(<FinalComponent />);
---
+++ README.md
@@ -0,0 +1,20 @@
+# React Knowledge
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm-written in c++
+- Caching-Faster Builds
+- Imag Optimizations
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support Old browsers
+- Diaognastics
+- Error Handling
+- Tree Shaking - remove ununsed code
+- Different dev and prod bundles
